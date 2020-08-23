91a04a7ce0434569efaab0fbf61a4a50cbd0dfcd
Jiwon Choi <jiwoncho@buffalo.edu>
2020-08-24 05:04:05 +0900

    implemented binary search in python; simple test cases added in main()

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+def binary_search (sorted_list, begin, end, key):
+    if (begin > end):
+        print ("ERROR: Index Out of Bound") 
+        return None
+    
+    middle_index = (begin + end) // 2
+    middle_elem = sorted_list[middle_index]
+    if (key < middle_elem):
+        return binary_search(sorted_list, begin, middle_index - 1, key)
+    elif (key > middle_elem):
+        return binary_search(sorted_list, middle_index + 1, end, key)
+    else:
+        return middle_index
+
+if __name__ == '__main__':
+    print ("Index of 15:", binary_search([1,3,8,11,15,17,20], 0, 7, 15))
+    print ("Index of 1:", binary_search([1,3,8,11,15,17,20], 0, 7, 1))
+    print ("Index of 20:", binary_search([1,3,8,11,15,17,20], 0, 7, 20))
+    print ("Index of 11:", binary_search([1,3,8,11,15,17,20], 0, 7, 11))
